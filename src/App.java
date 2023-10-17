5933b50c4589058313527a1ededd83dc16d11cef
edc3 <edc3@test.com>
2023-10-17 15:06:37 -0600

    abstraccion no terminada

--- src/App.java
+++ src/App.java
@@ -1,6 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        Motocicleta susukMotocicleta = new Motocicleta("deportiva", 02, 250.50);
+        Motocicleta susukMotocicleta = new Motocicleta("Deportiva", 2, 250.50);
         System.out.println(susukMotocicleta.getTipo());
     }
 }
